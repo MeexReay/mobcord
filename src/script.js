4ddc8ff1ff17a3140f00aa60d15666702198bf3e
MeexReay <meexreay@gmail.com>
2025-07-23 19:31:53 +0300

    add some animations + add settings menu support

--- src/script.js
+++ src/script.js
@@ -68,20 +68,63 @@ function leaveChatState() {
   // querySelect('[class^="search_"]', (o) => o.style.display = null);
 }
 
-function onHorizSwipe(from_x, to_x) {
-  if (uiState == "default" && from_x > to_x && from_x > 0.5) {
-    leaveDefaultState()
-    enterChatState()
-  }
+function enterOptionsState() {
+  querySelect('[class^="layer_"]:nth-of-type(1)', o => {
+    o.setAttribute("aria-hidden", "true");
+    o.style.opacity = "0";
+    o.style.display = "none";
+  });
+  querySelect('[class^="layer_"]:nth-of-type(2)', o => {
+    o.setAttribute("aria-hidden", "false");
+    o.style.opacity = null;
+    o.style.display = null;
+  });
   
-  if (uiState == "chat" && from_x < to_x && from_x < 0.5) {
-    leaveChatState()
+  uiState = "options"
+
+  let leaveButton = document.createElement("div")
+  leaveButton.innerHTML = '<svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24"><path fill="currentColor" d="M17.3 18.7a1 1 0 0 0 1.4-1.4L13.42 12l5.3-5.3a1 1 0 0 0-1.42-1.4L12 10.58l-5.3-5.3a1 1 0 0 0-1.4 1.42L10.58 12l-5.3 5.3a1 1 0 1 0 1.42 1.4L12 13.42l5.3 5.3Z" class=""></path></svg>'
+  leaveButton.children[0].style.color = "var(--icon-tertiary)";
+  leaveButton.style.cursor = "pointer";
+  leaveButton.style.position = "fixed";
+  leaveButton.style.zIndex = "101";
+  leaveButton.style.color = "var(--interactive-normal)";
+  leaveButton.style.top = "15px";
+  leaveButton.style.left = "15px";
+  leaveButton.style.width = "36px";
+  leaveButton.style.height = "36px";
+  leaveButton.style.borderRadius = "50%";
+  leaveButton.style.borderStyle = "solid";
+  leaveButton.style.borderWidth = "2px";
+  leaveButton.style.display = "flex";
+  leaveButton.style.alignItems = "center";
+  leaveButton.style.justifyContent = "center";
+  leaveButton.id = "leave-options-button";
+  
+  leaveButton.onclick = () => {
+    leaveOptionsState()
     enterDefaultState()
   }
+  
+  document.body.appendChild(leaveButton)
+}
+
+function leaveOptionsState() {
+  querySelect('[class^="layer_"]:nth-of-type(1)', o => {
+    o.setAttribute("aria-hidden", "false");
+    o.style.opacity = null;
+    o.style.display = null;
+  });
+  querySelect('[class^="layer_"]:nth-of-type(2)', o => {
+    o.setAttribute("aria-hidden", "true");
+    o.style.opacity = "0";
+    o.style.display = "none";
+  });
+  
+  document.body.querySelector('[id="leave-options-button"]').remove()
 }
 
 let startSwipe = null
-let startSwipeDate = null
 
 function doAlways() {
   querySelect('[class^="sidebarResizeHandle_"]', (o) => o.remove());
@@ -89,9 +132,7 @@ function doAlways() {
 
   document.ondragstart = () => { return false; };
   
-  let on_click = o => {
-    console.log(o.target)
-    
+  document.addEventListener("click", o => {
     if (o.target.closest('[class^="link_"]')) {
       if (uiState == "default") {
         leaveDefaultState();
@@ -99,12 +140,15 @@ function doAlways() {
       }
     }
     
-    // if (o.target.closest('div[data-list-item-id^="guildsnav"]')) {
-      // document.addEventListener("click", on_click);
-    // }
-  };
+    if (o.target.closest('[class^="buttons_"] > button:nth-of-type(2)')) {
+      if (uiState == "default") {
+        console.log("enter options menu")
+        leaveDefaultState();
+        enterOptionsState();
+      }
+    }
+  }, true);
   
-  document.addEventListener("click", on_click, true);
 
   let styles = document.createElement("style")
   styles.innerHTML = `
@@ -125,22 +169,161 @@ function doAlways() {
       display: none;
     }
     
-    [class^="layerContainer_"] {
+    [class^="popout_"] {
       display: none;
+    }
+    
+    [class^="focusLock_"] > div,
+    [class^="focusLock_"] > div > div,
+    [class^="focusLock_"] > div > div > div {
+      max-width: 100%;
+      width: 100%;
+    }
+
+    [class^="focusLock_"] > div > div > div > [class^="body_"] > div:nth-of-type(2) > [class^="scroller"] > section:nth-of-type(2) > ul,
+    [class^="focusLock_"] > div > div > div > [class^="body_"] > div:nth-of-type(2) > [role="tablist"] {
+      flex-wrap: wrap;
+      gap: 10px;
+    }
+    
+    [class^="focusLock_"] > div > div > div > [class^="body_"] > div:nth-of-type(2) > [class^="scroller"] > section:nth-of-type(2) > ul > * {
+      margin-left: 0;
+    }
+    
+    [class^="layer_"]:nth-of-type(2) {
+      overflow: scroll;
     }
   `;
   document.body.appendChild(styles);
 
   document.body.addEventListener("mousedown", (event) => {
-    if (event.button == 0) {
-      startSwipe = [event.clientX, event.clientY];
-      startSwipeDate = Date.now()
+    if (event.button == 0 && startSwipe == null) {
+      if (uiState == "default" && event.clientX / document.documentElement.clientWidth > 0.6) {
+        startSwipe = event.clientX;
+        
+        querySelect('[class^="base_"]', (o) => o.style.gridTemplateColumns = "[start] min-content [guildsEnd] 1fr [channelsEnd] 0fr [end]");
+
+        querySelect('[class^="page_"]', (o) => {
+          o.style.width = "100vw";
+          o.style.right = "-100%";
+          o.style.position = "absolute";
+          o.style.zIndex = "10";
+        });
+        querySelect('[class^="sidebar_"]', (o) => {
+          o.style.width = "100vw";
+          o.style.right = "0%";
+        });
+      }
+      
+      if (uiState == "chat" && event.clientX / document.documentElement.clientWidth < 0.1) {
+        startSwipe = event.clientX;
+        
+        querySelect('[class^="base_"]', (o) => o.style.gridTemplateColumns = "[start] min-content [guildsEnd] 1fr [channelsEnd] 0fr [end]");
+        querySelect('[class^="panels_"]', (o) => o.style.display = null);
+        querySelect('[class^="wrapper_"]', (o) => o.style.width = null);
+
+        querySelect('[class^="page_"]', (o) => {
+          o.style.width = "100vw";
+          o.style.right = "0%";
+          o.style.position = "absolute";
+          o.style.zIndex = "10";
+        });
+        querySelect('[class^="sidebar_"]', (o) => {
+          o.style.width = "100vw";
+          o.style.right = "0%";
+        });
+      }
+
+      if (startSwipe) {
+        let noSelectStyle = document.createElement("style");
+        noSelectStyle.innerHTML = `* {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}`;
+        noSelectStyle.id = "no-select-style";
+        document.body.appendChild(noSelectStyle);
+      }
+    }
+  }, true);
+  
+  document.body.addEventListener("mousemove", (event) => {
+    if (event.button == 0 && startSwipe != null) {
+      if (uiState == "default" &&
+          startSwipe / document.documentElement.clientWidth > 0.6 &&
+          startSwipe > event.clientX) {
+        let percent = Math.min((startSwipe - event.clientX) / document.documentElement.clientWidth / 0.4 * 100, 100)
+
+        querySelect('[class^="page_"]', (o) => {
+          o.style.right = (percent - 100) + "vw";
+        });
+      }
+      if (uiState == "chat" &&
+          startSwipe / document.documentElement.clientWidth < 0.1 &&
+          startSwipe < event.clientX) {
+        let percent = Math.min((event.clientX - startSwipe) / document.documentElement.clientWidth / 0.4 * 100, 100)
+
+        querySelect('[class^="page_"]', (o) => {
+          o.style.right = (0 - percent) + "vw";
+        });
+      }
     }
   });
   
   document.body.addEventListener("mouseup", (event) => {
-    if (event.button == 0 && startSwipe != null && startSwipeDate != null && Date.now() - startSwipeDate < 5000) {
-      onHorizSwipe(startSwipe[0] / document.documentElement.clientWidth, event.clientX / document.documentElement.clientWidth)
+    if (event.button == 0 && startSwipe != null) {
+      let from_x = startSwipe / document.documentElement.clientWidth;
+      let to_x = event.clientX / document.documentElement.clientWidth;
+
+      startSwipe = null;
+
+      document.body.querySelector('[id="no-select-style"]').remove();
+      
+      if (uiState == "default") {
+        querySelect('[class^="base_"]', (o) => o.style.gridTemplateColumns = "[start] min-content [guildsEnd] 1fr [channelsEnd] 0fr [end]");
+        
+        querySelect('[class^="page_"]', (o) => {
+          o.style.width = null;
+          o.style.right = null;
+          o.style.position = null;
+          o.style.zIndex = null;
+        });
+        querySelect('[class^="sidebar_"]', (o) => {
+          o.style.width = null;
+          o.style.right = null;
+        });
+
+        if (from_x > to_x && from_x > 0.6 && from_x - to_x > 0.2) {
+          leaveDefaultState();
+          enterChatState();
+        }
+      }
+  
+      if (uiState == "chat") {
+        querySelect('[class^="base_"]', (o) => o.style.gridTemplateColumns = "[start] 0fr [guildsEnd] 0fr [channelsEnd] 1fr [end]");
+        querySelect('[class^="panels_"]', (o) => o.style.display = "none");
+        querySelect('[class^="wrapper_"]', (o) => o.style.width = "0");
+        
+        querySelect('[class^="page_"]', (o) => {
+          o.style.width = null;
+          o.style.right = null;
+          o.style.position = null;
+          o.style.zIndex = null;
+        });
+        querySelect('[class^="sidebar_"]', (o) => {
+          o.style.width = null;
+          o.style.right = null;
+        });
+        
+        if (from_x < to_x && from_x < 0.1 && to_x - from_x > 0.2) {
+          leaveChatState();
+          enterDefaultState();
+        }
+      }
     }
   })
 }
